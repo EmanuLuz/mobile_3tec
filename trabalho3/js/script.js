d7c23c8e15891a46be3b02c34f9575d52db1b1c0
EmanuLuz <emanueli.bessa@escola.pr.gov.br>
2026-04-14 07:41:05 -0300

    terminei o codigo de medias

--- trabalho3/js/script.js
+++ trabalho3/js/script.js
@@ -3,28 +3,40 @@ console.log("Programa para calcular média dos alunos");
 
 function calcularMedia() {
 
-    let nome_aluno = document.getElementById("nome_aluno").value;
-    console.log(nome_aluno);
+    do{
 
-    let primeira_nota = Number(document.getElementById("primeira_nota").value);
-    console.log(primeira_nota);
-    
-    let segunda_nota = Number(document.getElementById("segunda_nota").value);
-    console.log(segunda_nota);
-    
-    let terceira_nota = Number(document.getElementById("terceira_nota").value);
-    console.log(terceira_nota);
+        let nome_aluno = document.getElementById("nome_aluno").value;
+        console.log(nome_aluno);
 
-    let resultado = (primeira_nota + segunda_nota + terceira_nota) / 3;
-    console.log(resultado);
+        let primeira_nota = Number(document.getElementById("primeira_nota").value);
+        console.log(primeira_nota);
+        
+        let segunda_nota = Number(document.getElementById("segunda_nota").value);
+        console.log(segunda_nota);
+        
+        let terceira_nota = Number(document.getElementById("terceira_nota").value);
+        console.log(terceira_nota);
 
-    let resposta = document.getElementById("resposta").value;
+        let resultado = (primeira_nota + segunda_nota + terceira_nota) / 3;
+        console.log(resultado);
 
-    if (resposta == "S"){
-        console.log("Continua");
-    } else {
-        console.log("Pare");
-    }
+        let resposta = document.getElementById("resposta").value;
+
+        if (resposta == "S"){
+            console.log("Continua");
+        } else {
+            console.log("Pare");
+        }
+
+        console.log(resultado);
+        document.getElementById("nome_aluno").value = "";
+        document.getElementById("primeira_nota").value = "";
+        document.getElementById("segunda_nota").value = "";
+        document.getElementById("terceira_nota").value = "";
+        document.getElementById("resposta").value = "";
+
+    } while ( resposta == "S" );
+
+
 }
-
 
